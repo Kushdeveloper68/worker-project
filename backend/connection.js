d7b3ddee1097671fe9f23797769410c5f0aef038
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <154511616+Kushdeveloper68@users.noreply.github.com>
2025-08-27 09:06:21 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 async function  mongodbConnection(url) {
-    mongoose.connect(url)
+    mongoose.connect(url,  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+      tlsInsecure: false,
+      serverSelectionTimeoutMS: 10000,
+    })
 }
 module.exports = {
   mongodbConnection
